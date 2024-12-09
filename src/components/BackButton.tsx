17542dc6e1144c8e11ada091a829d28cf46e219f
Kyungbae Min <kbmin1129@gmail.com>
2024-12-09 17:35:33 +0900

    BackButton history 기반 조건 추가

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,11 +1,40 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { IoMdArrowBack } from 'react-icons/io';
-import { IconButton } from '@chakra-ui/react';
+import { IconButton, IconButtonProps } from '@chakra-ui/react';
+import { ROUTES } from '@/constants/routes';
+import { useCallback } from 'react';
 
-const BackButton = () => {
+type BackButtonProps = {
+  onBack?: () => void;
+} & IconButtonProps;
+
+const BackButton = ({ onBack, ...props }: BackButtonProps) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const onClickBack = useCallback(() => {
+    if (onBack) onBack();
+    if (location.pathname === ROUTES.home) return;
+
+    if (window.history.length > 1) {
+      // 브라우저의 이전 기록 확인
+      const previousUrl = document.referrer; // 이전 페이지 URL
+      const currentDomain = window.location.origin;
+
+      if (previousUrl && previousUrl.startsWith(currentDomain)) {
+        // 이전 URL이 같은 도메인 내라면 뒤로가기
+        window.history.back();
+      } else {
+        // 외부 URL이거나 이전 기록이 없을 경우 홈으로 이동
+        navigate(ROUTES.home);
+      }
+    } else {
+      // 히스토리 스택이 없으면 홈으로 이동
+      navigate(ROUTES.home);
+    }
+  }, [location.pathname, navigate, onBack]);
+
   return (
-    <IconButton onClick={() => navigate(-1)} position="fixed" top={4} left={4}>
+    <IconButton onClick={onClickBack} {...props}>
       <IoMdArrowBack />
     </IconButton>
   );
