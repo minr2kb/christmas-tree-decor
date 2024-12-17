aa312731d8e3912882a7831e5a09dab67ca2090c
Kyungbae Min <kbmin1129@gmail.com>
2024-12-17 22:27:17 +0900

    비회원 컨트롤 제한

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,7 +1,7 @@
 import { Flex, IconButton } from '@chakra-ui/react';
 import { memo, useMemo } from 'react';
 import { LuMenu } from 'react-icons/lu';
-import { MenuContent, MenuRoot, MenuItem, MenuTrigger, MenuItemCommand, MenuSeparator } from './ui/menu';
+import { MenuContent, MenuRoot, MenuItem, MenuTrigger, MenuItemCommand, MenuSeparator, MenuItemGroup } from './ui/menu';
 import useKeyPress from '@/hooks/util/useKeyPress';
 
 import useControls from '@/hooks/logic/useControls';
@@ -39,57 +39,64 @@ const Controls = memo(({ toggleFullScreen, treeId }: ControlsProps) => {
   const MenuItems = useMemo(
     () => (
       <MenuContent bgColor={'bg.emphasized'}>
-        <MenuItem value="toggle-triangle" onClick={menuHandlers.toggleTriangle}>
-          삼각형 토글
-          <MenuItemCommand>T</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="toggle-count" onClick={menuHandlers.toggleCount}>
-          카운트 토글
-          <MenuItemCommand>C</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="toggle-snow" onClick={menuHandlers.toggleSnow}>
-          눈 토글
-          <MenuItemCommand>S</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="toggle-star" onClick={menuHandlers.toggleStar}>
-          별 토글
-          <MenuItemCommand>X</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="toggle-title" onClick={menuHandlers.toggleTitle}>
-          중앙 타이틀 토글
-          <MenuItemCommand>T</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="add-1" onClick={addTest1}>
-          1개 추가 <MenuItemCommand>A</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="add-50" onClick={addTest50}>
-          50개 추가 <MenuItemCommand>D</MenuItemCommand>
-        </MenuItem>
-
-        <MenuItem value="toggle-fullscreen" onClick={toggleFullScreen}>
-          전체화면 토글 <MenuItemCommand>F</MenuItemCommand>
-        </MenuItem>
-        <MenuSeparator />
-        <MenuItem value="generate-qr" onClick={generateQR}>
-          꾸미기 QR코드 생성 <MenuItemCommand>Q</MenuItemCommand>
-        </MenuItem>
-        <MenuItem value="copy-send-link" onClick={copySendLink}>
-          꾸미기 링크 복사 <MenuItemCommand>L</MenuItemCommand>
-        </MenuItem>
-        <MenuSeparator />
-        <MenuItem value="home" onClick={onClickHome}>
-          홈으로
-        </MenuItem>
-
-        {isOwner ? (
-          <MenuItem value="delete-tree" onClick={onClickRemoveTree} color="fg.error">
-            트리 삭제
-          </MenuItem>
-        ) : (
-          <MenuItem value="login" onClick={() => openLoginModal()}>
-            로그인
-          </MenuItem>
+        {isOwner && (
+          <MenuItemGroup>
+            <MenuItem value="toggle-triangle" onClick={menuHandlers.toggleTriangle}>
+              삼각형 토글
+              <MenuItemCommand>T</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="toggle-count" onClick={menuHandlers.toggleCount}>
+              카운트 토글
+              <MenuItemCommand>C</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="toggle-snow" onClick={menuHandlers.toggleSnow}>
+              눈 토글
+              <MenuItemCommand>S</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="toggle-star" onClick={menuHandlers.toggleStar}>
+              별 토글
+              <MenuItemCommand>X</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="toggle-title" onClick={menuHandlers.toggleTitle}>
+              중앙 타이틀 토글
+              <MenuItemCommand>T</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="add-1" onClick={addTest1}>
+              1개 추가 <MenuItemCommand>A</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="add-50" onClick={addTest50}>
+              50개 추가 <MenuItemCommand>D</MenuItemCommand>
+            </MenuItem>
+            <MenuItem value="toggle-fullscreen" onClick={toggleFullScreen}>
+              전체화면 토글 <MenuItemCommand>F</MenuItemCommand>
+            </MenuItem>
+          </MenuItemGroup>
         )}
+
+        {isOwner && <MenuSeparator />}
+        <MenuItemGroup>
+          <MenuItem value="generate-qr" onClick={generateQR}>
+            꾸미기 QR코드 생성 <MenuItemCommand>Q</MenuItemCommand>
+          </MenuItem>
+          <MenuItem value="copy-send-link" onClick={copySendLink}>
+            꾸미기 링크 복사 <MenuItemCommand>L</MenuItemCommand>
+          </MenuItem>
+        </MenuItemGroup>
+        <MenuSeparator />
+        <MenuItemGroup>
+          <MenuItem value="home" onClick={onClickHome}>
+            홈으로
+          </MenuItem>
+          {isOwner ? (
+            <MenuItem value="delete-tree" onClick={onClickRemoveTree} color="fg.error">
+              트리 삭제
+            </MenuItem>
+          ) : (
+            <MenuItem value="login" onClick={() => openLoginModal()}>
+              로그인
+            </MenuItem>
+          )}
+        </MenuItemGroup>
       </MenuContent>
     ),
     // eslint-disable-next-line react-hooks/exhaustive-deps
